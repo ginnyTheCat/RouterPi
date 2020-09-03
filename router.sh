69c500f11db175358958246938ba43be95d2a014
ginnyTheCat <ginnythecat@lelux.net>
2020-09-03 13:36:30 +0200

    Fixed interface names

--- router.sh
+++ router.sh
@@ -1,12 +1,12 @@
 COUNTRY=DE
 
-INTERNAL_WLAN=wlan0
-EXTERNAL_WLAN=wlan1
+INTERNAL_WLAN=aa:bb:cc:dd:ee:ff
+EXTERNAL_WLAN=aa:bb:cc:dd:ee:ee
 RANGE_WLAN=1
 CHANNEL=7
 
-INTERNAL_ETH=eth1
-EXTERNAL_ETH=eth0
+INTERNAL_ETH=11:22:33:44:55:66
+EXTERNAL_ETH=11:22:33:44:66:66
 RANGE_ETH=2
 
 # Install packets
@@ -20,16 +20,22 @@ orig_file() {
     sudo cp $1.orig $1
 }
 
+# Fix interfaces
+echo "SUBSYSTEM==\"net\", ACTION==\"add\", DRIVERS==\"?*\", ATTR{address}==\"$EXTERNAL_WLAN\", ATTR{type}==\"1\", NAME=\"wlan0\"
+SUBSYSTEM==\"net\", ACTION==\"add\", DRIVERS==\"?*\", ATTR{address}==\"$INTERNAL_WLAN\", ATTR{type}==\"1\", NAME=\"wlan1\"
+SUBSYSTEM==\"net\", ACTION==\"add\", DRIVERS==\"?*\", ATTR{address}==\"$EXTERNAL_ETH\", ATTR{type}==\"1\", NAME=\"eth0\"
+SUBSYSTEM==\"net\", ACTION==\"add\", DRIVERS==\"?*\", ATTR{address}==\"$INTERNAL_ETH\", ATTR{type}==\"1\", NAME=\"eth1\"" | sudo tee /etc/udev/rules.d/70-persistent-net.rules
+
 # Set IP static
 orig_file /etc/dhcpcd.conf
-echo "interface $INTERNAL_WLAN
+echo "interface wlan1
 static ip_address=192.168.$RANGE_WLAN.1/24
-interface $INTERNAL_ETH
+interface eth1
 static ip_address=192.168.$RANGE_ETH.1/24" | sudo tee -a /etc/dhcpcd.conf
 
 # Create DHCP config
-echo "dhcp-range=$INTERNAL_WLAN,192.168.$RANGE_WLAN.2,192.168.$RANGE_WLAN.255,255.255.255.0,24h
-dhcp-range=$INTERNAL_ETH,192.168.$RANGE_ETH.2,192.168.$RANGE_ETH.255,255.255.255.0,24h" | sudo tee /etc/dnsmasq.conf
+echo "dhcp-range=wlan1,192.168.$RANGE_WLAN.2,192.168.$RANGE_WLAN.255,255.255.255.0,24h
+dhcp-range=wlan1,192.168.$RANGE_ETH.2,192.168.$RANGE_ETH.255,255.255.255.0,24h" | sudo tee /etc/dnsmasq.conf
 
 # Get WiFi settings
 echo -n "SSID: "
@@ -38,7 +44,7 @@ echo -n "Password: "
 read PASSWORD
 
 # Create WiFi config
-echo "interface=$INTERNAL_WLAN
+echo "interface=wlan1
 ssid=$SSID
 channel=$CHANNEL
 hw_mode=g
@@ -63,9 +69,10 @@ orig_file /etc/sysctl.conf
 echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf
 
 orig_file /etc/rc.local
-sudo iptables -t nat -A POSTROUTING -o $EXTERNAL_WLAN -j MASQUERADE
-sudo iptables -t nat -A POSTROUTING -o $EXTERNAL_ETH -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
-sudo iptables-save >/etc/iptables.ipv4.nat
-sudo sed -i '/^exit 0/i iptables-restore < /etc/iptables.ipv4.nat
-/home/pi/net_led.sh &' /etc/rc.local
+sudo iptables-save | sudo tee /etc/iptables.ipv4.nat
+
+orig_file /etc/rc.local
+sudo sed -i '/^exit 0/i iptables-restore </etc/iptables.ipv4.nat\n/home/pi/net_led.sh &' /etc/rc.local
